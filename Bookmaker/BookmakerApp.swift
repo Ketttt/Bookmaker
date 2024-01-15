ba6c578aba08ad165cb088ea25990e55d97a522f
Екатерина Иванова <ei851925@gmail.com>
2024-01-15 14:26:35 +0300

    Initial Commit

---
+++ Bookmaker/BookmakerApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookmakerApp.swift
+//  Bookmaker
+//
+//  Created by Katerina Ivanova on 15.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct BookmakerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
